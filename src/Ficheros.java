f6920f075e3d84bb11eb79ff0af7953427fb6c5f
Robert Voong <rvoong1408@gmail.com>
2026-08-25 18:47:02 +0200

    metodo para obtener estadisticas de los n ultimos intentos

--- src/Ficheros.java
+++ src/Ficheros.java
@@ -4,19 +4,45 @@ import java.io.PrintWriter;
 import java.io.BufferedReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.io.FileReader;
+
 public class Ficheros {
     private PrintWriter writer;
     private BufferedReader reader;
 
     //ponemos estos objetos pero antes de meterlos en el constructor hay que inicializarlos con un FileReader y con un BufferdReader + filewriter
-    public Ficheros(PrintWriter escritor, BufferedReader lector){
+    public Ficheros(PrintWriter escritor){ //BufferedReader lector
         this.writer = escritor;
-        this.reader = lector;
+        // this.reader = lector;
     }
 
     public void escribir(String informacion) throws IOException{ writer.print(informacion); }
 
     // String file = Files.readString(Path.of(ruta));
     //metodo para leer el archivo de intentos
-    public String[] leerIntentos(String ruta) throws IOException{ return Files.readString(Path.of(ruta)).split(Fechas.SEPARATOR); }
+    private String[] leerIntentos(String ruta) throws IOException{ return Files.readString(Path.of(ruta)).split(Fechas.SEPARATOR); }
+
+    public String[] ultimasEstadisticas(String ruta, int numIntentos) throws IOException{
+        String[] fichero = leerIntentos(ruta);
+        double segundos = 0.0;
+        double exitos = 0.0;
+        ;
+        //para que no se rompa si el usuario es subnormal
+        int start = Math.max(0, fichero.length - numIntentos - 1);
+        //bucle for para  acumular las estadisticas
+        for(int i = fichero.length - 1; i > ((fichero.length - 1) - numIntentos); i--){
+            BufferedReader reader = new BufferedReader(new FileReader(fichero[i]));
+            //saltamos primeras dos lineas de la fecha y el dia de la semana
+            reader.readLine();
+            reader.readLine();
+            // exitos += Double.parseDouble(reader.readLine());
+            if("true".equals(reader.readLine())) exitos += 1;
+            segundos += Double.parseDouble(reader.readLine());
+        }
+        //calculamos los porcentajes
+        exitos /= numIntentos;
+        segundos /= numIntentos;
+
+        return  new String[]{String.valueOf(exitos), String.valueOf(segundos)};
+    }
 }
