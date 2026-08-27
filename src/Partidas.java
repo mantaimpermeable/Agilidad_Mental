b868c3db41fef41bd9cb265b389ae52ccf9a3d50
Robert Voong <rvoong1408@gmail.com>
2026-08-27 17:50:55 +0200

    Medir tiempo implementado

--- src/Partidas.java
+++ src/Partidas.java
@@ -1,6 +1,8 @@
 package src;
 import java.io.IOException;
 import java.util.Scanner;
+import java.time.Instant;
+import java.time.Duration;
 
 //Clase  para partidas o logicas en bloques sobre los distintos juegos, sin ninguna conexion
 public class Partidas {
@@ -8,13 +10,18 @@ public class Partidas {
     public boolean partidaFechas(Scanner teclado, String ruta, Fechas fecha){  
         //Le comunicamos al usuario la fecha
         System.out.println("Adivina que dia de la semana fue el: " + fecha.makeDate());
+        //preguntamos por una respuesta y medimo el tiempo
+        Instant inicio = Instant.now();
         String intento = teclado.next();
+        Instant fin = Instant.now();
+        double segundos = Duration.between(fin, inicio).toSeconds();
+
         boolean exito = fecha.isCorrect(intento);
         if(exito) System.out.println("Es correcto");
         else System.out.println("Error, el dia de la semana era un: " + fecha.getDiaSemana());
 
         try {
-            Ficheros.escribir(fecha.toRawString(exito, 0), ruta);
+            Ficheros.escribir(fecha.toRawString(exito, segundos), ruta);
         } catch (IOException e) {
             System.out.printf("An exception has ocurred %s", e.getMessage());
         }
