e600ed981761af59e291e3c1f621f3082f00cbd7
Robert Voong <rvoong1408@gmail.com>
2026-09-11 12:52:26 +0200

    Test para Fechas iniciados

---
+++ app/src/test/java/CL/E/Fechas/J/FechasTest.java
@@ -0,0 +1,13 @@
+package CL.E.Fechas.J;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FechasTest {
+    private Fechas fecha;
+     @Test void isCorrectTest() {
+        fecha = new Fechas();
+        String correcto = fecha.getDiaSemana();
+        assertTrue(fecha.isCorrect(correcto));
+     }
+}
---
+++ app/src/test/java/CL/E/Fechas/J/UtilidadesTest.java
@@ -0,0 +1,22 @@
+package CL.E.Fechas.J;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.Scanner;
+
+import org.junit.jupiter.api.Test;
+
+//test de la clase utilidades
+public class UtilidadesTest {
+
+    private Scanner scanner = new Scanner(System.in);
+    private static final String GEN_MSG = "LLamalava llamababa esto es un string general";
+
+    @Test void leerNumeroTest() {
+        assertEquals(5, Utilidades.leerNumero(scanner, GEN_MSG, 1, 10));
+        assertFalse(Utilidades.leerNumero(scanner, GEN_MSG, 1, 10) < 1);
+        assertFalse(Utilidades.leerNumero(scanner, GEN_MSG, 1, 10) > 10);
+        assertFalse(GEN_MSG.equals(Utilidades.leerNumero(scanner, GEN_MSG, 1, 10)));
+    }
+}
